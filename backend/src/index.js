1647d8b9b0235c1463285d5b8e33c53df86b2e41
Luiz Felipe Montuani e Silva <luiz.silva16@estudante.ufla.br>
2020-04-01 21:57:48 -0300

    Front-end da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,37 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-/** 
- * Rota / Recurso
- */
-
- /**
-  * Métodos HTTP:
-  * 
-  * GET: Buscar/listar uma informação do back-end
-  * POST: Criar uma informação no back-end
-  * PUT: Alterar uma informação no back=end
-  * DELETE: Deletar uma informaçção no back-end
-  */
-
-  /**
-   * Tipos de parâmetros
-   * 
-   * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
-   * Route Params: Parâmetros utilizados para identifcar recursos
-   * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
-   */
-
-   /**
-    * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
-    * NoSql: MongoDB, CouchDB, etc
-    */
-
-    /**
-     * Driver: SELECT * FROM users
-     * Query Builder: table('users').select('*').where( )
-     */
-
-
 app.listen(3333);
 
